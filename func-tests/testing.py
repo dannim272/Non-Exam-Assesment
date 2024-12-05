2e469ebba8e60c4d960af4035d9bb6e506afe1b6
Daniil <dmorozkin26@gmail.com>
2024-12-05 23:44:04 +0000

    batman isnt batmanning, WHAT

--- func-tests/testing.py
+++ func-tests/testing.py
@@ -1,4 +1,3 @@
-import requests
 import yfinance as yf
 import numpy as np
 import matplotlib.pyplot as plt
@@ -12,23 +11,6 @@ data = stock.history(period="1d", interval="1m", prepost=True)
 pre_market_data = data.between_time("8:00","9:30")
 y = pre_market_data.reset_index()
 y = np.array(y["Close"])
-
-b = 0
-o = 1
-s = 2
-p = 3
-r = 4
-u = 5
-
-while True:
-    if levels[o] < levels[b] and levels[b] < levels[s] and levels[s] > levels[p] and levels[p] > levels[r] and levels[r] > levels[u]:
-        print(f'{levels[o]} - {levels[b]} - {levels[s]}')
-    b += 1
-    o += 1
-    s += 1
-    p += 1
-    r += 1
-    u += 1
 
 x = []
 a = 9
