7e0c6846042186d47b5c3464e621166e7ce244a7
Daniil <dmorozkin26@gmail.com>
2024-12-07 23:47:44 +0000

    displays graph for resistances

--- terminal.py
+++ terminal.py
@@ -10,9 +10,13 @@ hours = 0
 minutes = 0
 g = 0
 l = 0
+# gainers = []
+# losers = []
 
 def main():
     # vars
+    # global gainers
+    # global losers 
     global g
     global l
     global hours
@@ -26,20 +30,19 @@ def main():
 
     # tckr labels
     tckr = tk.Label(root)
-    tckr1 = tk.Label(root)
-    tckr2 = tk.Label(root)
-    tckr3 = tk.Label(root)
-    tckr4 = tk.Label(root)
     tckr.grid(row=(4), column=1)
+    tckr1 = tk.Label(root)
     tckr1.grid(row=(5), column=1)
+    tckr2 = tk.Label(root)
     tckr2.grid(row=(6), column=1)
+    tckr3 = tk.Label(root)
     tckr3.grid(row=(7), column=1)
+    tckr4 = tk.Label(root)
     tckr4.grid(row=(8), column=1)
 
-    # check button
-    check_var1 = tk.IntVar()
-    check1 = tk.Checkbutton(root, variable = check_var1)
-    check1.grid(row=4, column=0)
+    # tckr button
+    tckr_button = tk.Button(root, text='Graph', command=lambda: plot())
+    tckr_button.grid(row=4, column=0)
 
     def timing():
         global hours
@@ -73,7 +76,10 @@ def main():
             price_label.config(text=price)
             price_label.after(200, ticker_price)
     def plot():
-        stock = yf.Ticker("aapl")
+        if clicked.get() == "$ Gainers":
+            stock = yf.Ticker(gainers[0])
+        if clicked.get() == "$ Losers":
+            stock = yf.Ticker(losers[0])
         levels = stock.history(period="1mo")
         levels = levels.reset_index()
         levels = np.array(levels["Close"])
@@ -92,7 +98,7 @@ def main():
         ax.plot(x, y)
         canvas = FigureCanvasTkAgg(sp_fig, root)
         canvas.draw()
-        canvas.get_tk_widget().grid(row=4,column=4)
+        canvas.get_tk_widget().grid(row=9,column=2)
 
 
     def submit(tckr,tckr1,tckr2,tckr3,tckr4):
@@ -103,15 +109,13 @@ def main():
         root.after(50, lambda: tckr3.config(text=''))
         root.after(50, lambda: tckr4.config(text=''))
         if clicked.get() == "$ Gainers":
-            root.after(50, lambda: check1.config(text=f'{losers[0]}: ${losers[1]}'))
-            # root.after(50, lambda: tckr.config(text=f'{gainers[0]}: ${gainers[1]}'))
+            root.after(50, lambda: tckr.config(text=f'{gainers[0]}: ${gainers[1]}'))
             root.after(50, lambda: tckr1.config(text=f'{gainers[2]}: ${gainers[3]}'))
             root.after(50, lambda: tckr2.config(text=f'{gainers[4]}: ${gainers[5]}'))
             root.after(50, lambda: tckr3.config(text=f'{gainers[6]}: ${gainers[7]}'))
             root.after(50, lambda: tckr4.config(text=f'{gainers[8]}: ${gainers[9]}'))
         if clicked.get() == "$ Losers":
-            root.after(50, lambda: check1.config(text=f'{losers[0]}: ${losers[1]}'))
-            # root.after(50, lambda: tckr.config(text=f'{losers[0]}: ${losers[1]}'))
+            root.after(50, lambda: tckr.config(text=f'{losers[0]}: ${losers[1]}'))
             root.after(50, lambda: tckr1.config(text=f'{losers[2]}: ${losers[3]}'))
             root.after(50, lambda: tckr2.config(text=f'{losers[4]}: ${losers[5]}'))
             root.after(50, lambda: tckr3.config(text=f'{losers[6]}: ${losers[7]}'))
@@ -169,7 +173,6 @@ def main():
     clicked.set("$ Gainers")
     drop = tk.OptionMenu(root, clicked, *screener_options).grid(row=3, column=0)
     button = tk.Button(root, text='Submit', command=lambda: submit(tckr,tckr1,tckr2,tckr3,tckr4)).grid(row=3, column=1)
-    button_cmp = tk.Button(root, text='Show Graph', command=lambda: plot()).grid(row=3, column=2)
 
     tk.mainloop()
 
